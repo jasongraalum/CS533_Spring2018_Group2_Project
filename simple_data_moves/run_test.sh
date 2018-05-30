989abd96d0784268dad95491146738daf9c00be2
Shikha Shah <shikha2@fab02.cecs.pdx.edu>
2018-05-30 16:13:19 -0700

    updated bash script

--- simple_data_moves/run_test.sh
+++ simple_data_moves/run_test.sh
@@ -2,10 +2,16 @@
 
 
 # Get a list of all files beginning with "case" but not *.cpp files
-tests=`ls -1 case* | grep -v cpp`
-
+tests =`ls -1 case* | grep -v cpp`
+loop_cout = (1000 10000 100000 1000000 1000000000)
 # Loop on list of test cases
 for test in $tests
 do
-    time perf stat -d -d -d -d ./$test 4096 $loop_cout 8 > $test.out
+    for loop in ${loop_cout[*]}
+    do
+        for test_run in {1..10}
+        do
+            time perf stat -d -d -d -d ./$test 4096 ${loop_cout[$index]} 8 > $test.out
+        done    
+    done    
 done
