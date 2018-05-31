4d16fa5c81495b408c67ba4c6f2223dd761430d4
Jason Graalum <jasongraalum@gmail.com>
2018-05-30 19:12:08 -0700

    Enhancements to run.sh

--- simple_data_moves/run_test.sh
+++ simple_data_moves/run_test.sh
@@ -2,16 +2,20 @@
 
 
 # Get a list of all files beginning with "case" but not *.cpp files
-tests =`ls -1 case* | grep -v cpp`
-loop_cout = (1000 10000 100000 1000000 1000000000)
+tests=`ls -1 case* | grep -v cpp`
 # Loop on list of test cases
 for test in $tests
 do
-    for loop in ${loop_cout[*]}
+    for loop in 1000 10000 100000 1000000 1000000000
     do
-        for test_run in {1..10}
+        for test_run in {1..2}
         do
-            time perf stat -d -d -d -d ./$test 4096 ${loop_cout[$index]} 8 > $test.out
+            ((loop_count=${loop}*1000))
+            out_file=out_${test}.${loop}.${test_run}
+	    if [ ! -f ${out_file} ]; then
+              rm out_${test}.${loop}.${test_run}
+   	    fi
+            { time perf stat -d -d -d -d ./$test 4096 ${loop_count} 8 ; } &> out_${test}.${loop}.${test_run}
         done    
     done    
 done
