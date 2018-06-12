d979de7b27db7c008ae2c3e9a7f164eeffd4b512
Jason Graalum <jasongraalum@gmail.com>
2018-06-12 15:16:09 -0700

    Added new automation script

---
+++ mapreduce/run_test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Get a list of all files beginning with "case" but not *.cpp files
+super_args={ 0, 1, 2, 3 }
+echo ${super_args}
+# Loop on list of test cases
+for test in $super_args
+do
+        for test_run in {1..10}
+        do
+            out_file=data/mapr_out_${test}.${test_run}
+
+            if [ -f ${out_file} ]; then
+                rm ${out_file}
+            fi
+
+#            read start_power
+            fswebcam -r 1280x720 --no-banner ${out_file}.start.jpg
+
+            { time /usr/src/linux/tools/perf/perf stat -d -d -d -d python ./supervisor.py $test; } &> ${out_file}
+
+#           read end_power
+            fswebcam -r 1280x720 --no-banner ${out_file}.end.jpg
+
+            echo "Starting Power = "${start_power} >> ${out_file}
+            echo "Ending Power = "${end_power} >> ${out_file}
+
+    done    
+done
